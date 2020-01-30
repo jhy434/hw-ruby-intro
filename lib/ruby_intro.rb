8032a3ee533b9f8f7f4ab95868c5bcb19edb6c10
jhy434 <jhy434@tamu.edu>
2020-01-30 12:56:53 -0600

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,52 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+	# YOUR CODE HERE
+	i = 0
+	s = 0
+	loop do
+		if i == arr.length
+			break
+		end
+		s += arr[i]
+		i += 1
+	end
+	sum = s
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+	max = 0
+	max2 = 0
+	i = 0
+	loop do
+		if i == arr.length
+			break
+		end
+		if arr[i] > max
+			max2 = max
+			max = arr[i]
+		end
+		i += 1
+	end
+	sum = max2 + max
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+	nlist = {}
+	i = 0
+	loop do
+		if i == arr.length
+			break
+		end
+		diff = n - arr[i]
+		if nlist.has_key?(diff)
+			return true
+		end
+		nlist[arr[i]] = 1
+	end
+	return false
 end
 
 # Part 2
@@ -33,3 +70,7 @@ end
 class BookInStock
 # YOUR CODE HERE
 end
+
+arr = [1, 3, 5, 7 ,8 ,9]
+sum(arr)
+
