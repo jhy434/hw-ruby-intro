07fe907490438422b8f096447105095cf0009846
EC2 Default User <ec2-user@ip-172-31-93-77.ec2.internal>
2020-01-30 19:06:50 +0000

    New Changes

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -18,14 +18,20 @@ end
 
 def max_2_sum arr
   # YOUR CODE HERE
-	max = 0
-	max2 = 0
+	max = -999
+	max2 = -999
 	i = 0
+	if arr.length == 0
+	   return 0 
+	end
+	if arr.length == 1
+	   return arr[0] 
+	end
 	loop do
 		if i == arr.length
 			break
 		end
-		if arr[i] > max
+		if (arr[i] >= max2)
 			max2 = max
 			max = arr[i]
 		end
@@ -47,6 +53,7 @@ def sum_to_n? arr, n
 			return true
 		end
 		nlist[arr[i]] = 1
+		i += 1
 	end
 	return false
 end
@@ -55,22 +62,76 @@ end
 
 def hello(name)
   # YOUR CODE HERE
+  a = ""
+  a = "Hello, " + name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+    if ((s[0] =~/[a-zA-Z]/) == nil)
+       return false 
+    end
+    if(s[0] == 'A' || s[0] == 'E' || s[0] == 'I' || s[0] == 'O' || s[0] == 'U' || s[0] == 'a' || s[0] == 'e' || s[0] == 'i' || s[0] == 'o' || s[0] == 'u' || s.length == 0)
+        return false
+    end
+    return true
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if s.length == 0
+     return false 
+  end
+  if ((s =~/[2-9a-zA-Z]/) != nil)
+  		return false
+  end
+  sum = 0
+  i = 0
+  loop do
+  		if i == s.length
+  			break
+  		end
+  		if((s[i].ord - 48) == 1)
+  			sum += 2 ** (s.length - i - 1)
+  		end
+  		i += 1
+  end
+  puts sum
+  if sum % 4 == 0
+  	return true
+  end
+  return false
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+@isbn = ""
+@price = 0.0
+def initialize(is,pr)
+	@isbn = is
+	@price = pr
+	if((@isbn.length == 0) || (@price <= 0.0))
+		puts "Arg"
+		raise ArgumentError.new("ERROR")
+	end
 end
 
-arr = [1, 3, 5, 7 ,8 ,9]
-sum(arr)
+def price_as_string
+	@price = @price.round(2)
+	p = @price.to_s
+	p = "$" + p
+	if(p.index('.') == nil)
+		p = p + ".00"
+	elsif(p.index('.') == p.length - 2)
+		p = p + "0"
+	else
+		return p
+	end
+end
 
+attr_accessor :price 
+attr_accessor :isbn 
+
+end
